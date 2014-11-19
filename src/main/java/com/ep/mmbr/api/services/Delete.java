722409e4dcc6f2321e66bfeabc57be62eabfd49b
pg092111 <pg092111@cwvsft25.ep.corp>
2014-11-18 19:47:13 -0800

    Added new script

--- src/main/java/com/ep/mmbr/api/services/Delete.java
+++ src/main/java/com/ep/mmbr/api/services/Delete.java
@@ -1,15 +1,45 @@
 package com.ep.mmbr.api.services;
 
+import static com.jayway.restassured.RestAssured.given;
+
 import org.json.simple.JSONObject;
 
 import com.jayway.restassured.response.Response;
+import com.jayway.restassured.specification.RequestSpecification;
 
+/**
+ * This class contains rest assured methods to send request and get response for
+ * http delete method
+ * 
+ * @author preethi
+ * 
+ */
 public class Delete implements Service {
-
+	
+	/**
+	 * This method send delete request with path parameters and returns the
+	 * response
+	 * 
+	 * @param token
+	 *            (salesforce token)
+	 * @param uri
+	 * @param parameters
+	 *            (pathparameters)
+	 */
 	@Override
 	public Response sendRequestAndgetResponse(JSONObject obj, String token) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		
+			RequestSpecification requestSpec = new RequestSpecificationBuilder()
+			.getRequestSpecification("PathParameter", obj.get("requestParameters"));
+
+	Response response = given().header("authorization", token).with()
+			.spec(requestSpec).when().delete((String) obj.get("uri"));
+	return response;
+
+		
 	}
 
-}
+	
+	}
+
